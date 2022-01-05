ccf2c3fba9b201f17d36c03a64ddb062e8cd0507
attiolli <attiolli@gmail.com>
2022-01-05 11:31:32 +0200

    A quick fix for rsync script

--- rsync/rsync_backup.sh
+++ rsync/rsync_backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -v -e "ssh -i ~/.ssh/id_rsa -p 22" --exclude-from='~/home_automation/rsync/rsync_backup_exclude.txt' --backup --backup-dir=`date +%Y.%m.%d` -a user@remoteserver:/remote/backup/path/ /local/backup/path
+rsync -v -e "ssh -i ~/.ssh/id_rsa -p 22" --exclude-from='~/home_automation/rsync/rsync_backup_exclude.txt' --backup --backup-dir=`date +%Y.%m.%d` -a user@remoteserver:/remote/backup/path/ /local/backup/path/
